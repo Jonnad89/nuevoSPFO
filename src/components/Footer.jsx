92df595ce5d878762704b67574644132985aa993
Jonnad89 <jonnadvwork@gmail.com>
2024-10-05 20:19:12 -0300

    logo en la pestaña

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,17 +6,7 @@ const Footer = () => {
             <div className="Footer">
                 <div className="container-footer">
                     <div className="row">
-                        <div className="col-md-6 col-lg-5 col-12 ft-1">
-                            {/* <h3><span>WALEED</span>CODES</h3>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum ea quo ex ullam laboriosam magni totam, facere eos iure voluptate.</p>
-                            <div className="footer-icons">
-                                <i class="fa-brands fa-facebook"></i>
-                                <i class="fa-brands fa-twitter"></i>
-                                <i class="fa-brands fa-instagram"></i>
-                                <i class="fa-brands fa-linkedin-in"></i>
-                            </div> */}
-                        </div>
-                        <div className="col-md-6 col-lg-3 col-12 ft-2">
+                    <div className="col-md-6 col-lg-3 col-12 ft-2">
                             {/* <h5>Quick Links</h5> */}
                             <ul>
                                 <li className="nav-item">
@@ -31,6 +21,17 @@ const Footer = () => {
                                
                             </ul>
                         </div>
+                        <div className="col-md-6 col-lg-5 col-12 ft-1">
+                            {/* <h3><span>WALEED</span>CODES</h3>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum ea quo ex ullam laboriosam magni totam, facere eos iure voluptate.</p>
+                            <div className="footer-icons">
+                                <i class="fa-brands fa-facebook"></i>
+                                <i class="fa-brands fa-twitter"></i>
+                                <i class="fa-brands fa-instagram"></i>
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </div> */}
+                        </div>
+                        
                         {/* <div className="col-md-6 col-lg-4 col-12 ft-3">
                             <h5>Quick Links</h5>
                             <p><i class="fa-solid fa-phone-volume"></i> +92 3121324083</p>
@@ -41,6 +42,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className='Last-footer'>
+                
                 <p>Desarrollado Por FullDev</p>
             </div>
         </>
