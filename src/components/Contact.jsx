b1aaccd3b7ef6dc4b872a8acc2b77db228a19d8f
juancarlosj01 <juancarlosj01@hotmail.com>
2024-10-08 21:14:23 +0200

    imagenes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import blur from "../assets/blur.png";
-import dados from "../assets/dados.png";
-import edificio from "../assets/edificio.jpg";
-import reunion from "../assets/reunion.png";
-import './Contact.css'
+//import React from "react";
+import img1 from "../assets/img1.jpg";
+import img2 from "../assets/img2.jpg";
+import img3 from "../assets/img3.jpg";
+import img4 from "../assets/img4.jpg";
+//import img5 from "../assets/img1.jpg";
+import "./Contact.css";
 const Contact = () => {
   return (
     <div className="car-con">
@@ -15,16 +16,16 @@ const Contact = () => {
         <div className="carousel-inner">
           <p className="carousel-text">Alianza de calidad, seguridad y éxito</p>
           <div className="carousel-item active">
-            <img src={blur} className="d-block w-100" alt="..." />
+            <img src={img1} className="d-block w-100" alt="..." />
           </div>
           <div className="carousel-item">
-            <img src={dados} className="d-block w-100" alt="..." />
+            <img src={img2} className="d-block w-100" alt="..." />
           </div>
           <div className="carousel-item">
-            <img src={edificio} className="d-block w-100" alt="..." />
+            <img src={img3} className="d-block w-100" alt="..." />
           </div>
           <div className="carousel-item">
-            <img src={reunion} className="d-block w-100" alt="..." />
+            <img src={img4} className="d-block w-100" alt="..." />
           </div>
         </div>
         <button
@@ -33,7 +34,10 @@ const Contact = () => {
           data-bs-target="#carouselExampleFade"
           data-bs-slide="prev"
         >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
           <span className="visually-hidden">Previous</span>
         </button>
         <button
@@ -42,58 +46,71 @@ const Contact = () => {
           data-bs-target="#carouselExampleFade"
           data-bs-slide="next"
         >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
           <span className="visually-hidden">Next</span>
         </button>
       </div>
       {/* FORM */}
       <div className="container">
-  <div className="row">
-    {/* <!-- Columna para el texto de contacto --> */}
-    <div className="col-md-6 contact-info">
-      <h3>CONTACTA CON <span>NOSOTROS</span></h3>
-      <p><strong>MADRID</strong></p>
-      <p>Paseo de la Castellana 259C Planta 18</p>
-      <p>Torre de Cristal, Madrid - España 28046</p>
-      <p>Email: info@spfo.es</p>
-      <p>Tel: +34 91 451 10 45</p>
-      <p>Fax: +34 91 119 05 01</p>
-      <br/>
-      <p><strong>SEVILLA</strong></p>
-      <p>Calle Mairena 61 1 - A-B</p>
-      <p>Alcalá De Guadaira, Sevilla - España 41500</p>
-    </div>
-    
-    {/* <!-- Columna para el formulario --> */}
-    <div className="col-md-6 form-container">
-      <div className="mb-3">
-        <label for="exampleFormControlInput1" className="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleFormControlInput1"
-          placeholder="name@example.com"
-        />
-      </div>
-      <div className="mb-3">
-        <label for="exampleFormControlTextarea1" className="form-label">
-          Example textarea
-        </label>
-        <textarea
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
-        ></textarea>
-      </div>
-      <div className="btn-enviar">
-      <button className="btn btn-primary" type="submit">Enviar</button>
-      </div>
-    </div>
-  </div>
-</div>
+        <div className="row">
+          {/* <!-- Columna para el texto de contacto --> */}
+          <div className="col-md-6 contact-info">
+            <h3>
+              CONTACTA CON <span>NOSOTROS</span>
+            </h3>
+            <p>
+              <strong>MADRID</strong>
+            </p>
+            <p>Paseo de la Castellana 259C Planta 18</p>
+            <p>Torre de Cristal, Madrid - España 28046</p>
+            <p>Email: info@spfo.es</p>
+            <p>Tel: +34 91 451 10 45</p>
+            <p>Fax: +34 91 119 05 01</p>
+            <br />
+            <p>
+              <strong>SEVILLA</strong>
+            </p>
+            <p>Calle Mairena 61 1 - A-B</p>
+            <p>Alcalá De Guadaira, Sevilla - España 41500</p>
+          </div>
 
+          {/* <!-- Columna para el formulario --> */}
+          <div className="col-md-6 form-container">
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
+            <div className="mb-3">
+              <label
+                htmlFor="exampleFormControlTextarea1"
+                className="form-label"
+              >
+                Example textarea
+              </label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+            </div>
+            <div className="btn-enviar">
+              <button className="btn btn-primary" type="submit">
+                Enviar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,10 +1,10 @@
 //import React from "react";
 import "./Inicio.css";
 import img1 from "../assets/img1.jpg";
-import img2 from "../assets/img1.jpg";
-import img3 from "../assets/img1.jpg";
-import img4 from "../assets/img1.jpg";
-import img5 from "../assets/img1.jpg";
+import img2 from "../assets/img2.jpg";
+import img3 from "../assets/img3.jpg";
+import img4 from "../assets/img4.jpg";
+import img5 from "../assets/img5.jpg";
 import Carousel from "./Carousel";
 
 const Inicio = () => {
