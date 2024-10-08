5d02370dc085604cd9b7d0a9ea9eeb416939c36b
Jonnad89 <jonnadvwork@gmail.com>
2024-10-08 13:02:34 -0300

    fonts

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "@fortawesome/free-brands-svg-icons"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,  // permite que el servidor escuche en la red local
     port: 3000,  // puerto que quieres utilizar
+  },
+ fonts: {
     build: {
       rollupOptions: {
-        external: {FontAwesomeIcon}
+        external: ["@fortawesome/free-brands-svg-icons"]
       }
     }
-  },
+  }
 })
-
-// ["@fortawesome/free-brands-svg-icons"]
